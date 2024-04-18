174cb63945e48ccf1cfc03d82b3eea729f2b152c
Ludovic Bellier <ludovic.bellier@gmail.com>
2024-04-18 16:08:23 -0700

    fix format

--- caiman/base/movies.py
+++ caiman/base/movies.py
@@ -1610,7 +1610,7 @@ def load(file_name: Union[str, List[str]],
             T = mov.timing.num_samples
             dims = mov.spacing.num_pixels
             # fr = 1e6 / mov.timing.period.to_usecs()
-	    fr = 1 / mov.timing.period.secs_float
+            fr = 1 / mov.timing.period.secs_float
 
             idx_frames = np.arange(T)
             if subindices is not None:
